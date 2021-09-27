3606af62968532f234d8e5cd09e21f4320611918
Mubarra-naz <mubarra.naz@square63.org>
2021-09-27 12:00:48 +0500

    adding files

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  resources :users
+
+  re
+  resources :tags
+  resources :guests
+  resources :comments
+  resources :categories
+  resources :articles
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
